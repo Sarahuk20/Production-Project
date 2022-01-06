eb6329b1cda985167fe11edd526a660a79aa4fae
Sarah Al-Aradah <47329118+Sarahuk20@users.noreply.github.com>
2022-01-06 14:40:42 +0000

    Add files via upload

---
+++ Party Website/js/valid.js
@@ -0,0 +1,25 @@
+//Validation Code For Input
+
+	var email=document.forms["form"]["email"];
+	var password=document.forms"form"]["password"];
+
+
+
+
+
+function validation(){
+	if (email.vlaue.length < 9) {
+	    email.style.border ="1px solid red";
+	    email.focus < 9)
+		alert ("login succesfully");
+		return false;
+	}
+	
+	else{
+		alert ("login falied");
+	}
+
+
+
+
+}
